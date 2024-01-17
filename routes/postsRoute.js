04ed110501cc58e2daa63874232071a4bc439dab
ankit prajapati <ap8424856@gmail.com>
2024-01-16 19:01:12 -0800

    basic CRUD controllers implemented

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -3,7 +3,11 @@ const postController = require("../controllers/postController");
 const router = require("express").Router();
 
 router.route("/post")
-    .get(postController.getAllPost)
     .post(postController.addPost)
+    .put(postController.updatePost)
+
+router.route("/post/:postId")
+    .get(postController.getPost)
+    .delete(postController.deletePost)
 
 module.exports = router;
